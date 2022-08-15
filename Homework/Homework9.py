eaa56bfec26f7013694c6a366a42c0790096b25d
StepanTWL <h3xp3ctb@gmail.com>
2022-08-15 17:54:42 +0300

    15.08.2022 Work

--- Homework/Homework9.py
+++ Homework/Homework9.py
@@ -1,9 +1,4 @@
-
-
-
-from multiprocessing.sharedctypes import Value
-
-
+"""
 account = {
   "id": 3136,
   "uid": "1359acc6-f07a-4a2a-984e-3fb809982948",
@@ -51,3 +46,31 @@ if countrie:
     print(f'INFO: {city} is a city in {countrie}')
 else:
     print(f'ERROR: Country for {city} not found')
+"""
+
+user = {
+    "id": 4170,
+    "uid": "5e941db5-9e0f-4f94-9fc5-734110c6be14",
+    "password": "SyUpfo1ljm",
+    "first_name": "Teresa",
+    "last_name": "Wehner",
+    "username": "teresa.wehner",
+    "email": "teresa.wehner@email.com",
+    "gender": "Non-binary",
+    "phone_number": "+674 424.561.2776",
+    "social_insurance_number": "637316241",
+    "date_of_birth": "1993-08-17"
+}
+user_new=dict()
+password=user.pop('password')
+last_name=user.pop('last_name')
+for i in user:
+    if i=='first_name':
+        user_new['secret'] = password
+    elif i=='username':
+        user_new['surname'] = last_name
+    user_new[i]=user[i]
+user.clear()
+for i in user_new:
+    user[i]=user_new[i]
+print()
