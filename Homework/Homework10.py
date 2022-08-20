5db84b4e3117c6dbfd3832af8eceb6b30062eaaa
StepanTWL <h3xp3ctb@gmail.com>
2022-08-20 07:47:29 +0300

    20.08.2022 home

--- Homework/Homework10.py
+++ Homework/Homework10.py
@@ -6,14 +6,72 @@ english_words = ('attack', 'bless', 'look', 'reckless', 'short', 'monster', 'tro
 
 for index, word in enumerate(english_words,1):
     print(f'Word № {index} = {word}')
+
+
+arr=list(enumerate([int(a) for a in input()],1))
+arr1=[]
+for i in range(len(arr)):
+    if not i%2:
+        if arr[i][1]*2 > 9:
+            arr1.append(arr[i][1]*2-9)
+        else:
+            arr1.append(arr[i][1]*2)
+    else:
+        arr1.append(arr[i][1])
+
+
+a=sum(arr1)
+a%=10
+if sum(arr1)%10:
+    print('False')
+else:
+    print('True')
+
+
+
+def keanu_reeves() ->str:
+    return print(f"YOU'RE BREATHTAKING")
+keanu_reeves()
+
+
+
+def summa_n(t:int) -> str:
+    S=sum(range(t+1))
+    return print(f"Я знаю, что сумма чисел от 1 до {t} равна {S}")
+
+summa_n(5)
+
+
+
+
+def check_password(s:str) -> str:
+    count=0
+    if len(s)<10:
+        return print('Easy peasy')
+    if s.islower():
+        return print('Easy peasy')
+    for i in s:
+        if i=='!' or i=='@' or i=='#' or i=='$' or i=='%' or i=='*' or i.isalnum():
+            continue
+        else:
+            return print('Easy peasy')
+    for i in s:
+        if i.isdecimal():
+            count += 1
+    if count<3:
+        return print('Easy peasy')
+    else:
+        return print('Perfect password')
+
+check_password(input())
 """
 
-arr=[int(a) for a in input()]
-sett=set(enumerate(arr,1))
-arr1=[]
-for i in range(1,len(sett)+1):
-    if i%2:
-        arr1.append(sett[i-1][1])
-    elif not i%2:
-        arr1.append(a if sett[i-1][1]*2>9 )
-print(sett)
+def count_letters(s:str) -> str:
+    N=0
+    K=0
+    for i in s:
+        if i.islower():
+            K+=1
+        elif i.isalpha():
+            N+=1
+    return print(f"Количество заглавных символов: {N}\nКоличество строчных символов: {K}")
