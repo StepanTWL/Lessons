ba95c96c81c36303669bf8315d54f24e96b1cc7b
StepanTWL <h3xp3ctb@gmail.com>
2022-08-29 21:26:07 +0300

    29.08.2022 home_1

--- Lesson55_pingpong.py
+++ Lesson55_pingpong.py
@@ -1,3 +1,4 @@
+import random
 import turtle
 
 winndow = turtle.Screen()
@@ -31,34 +32,76 @@ for i in range(25):
         border.forward(24)
         border.down()
 
-rockets=[]
-#for i in range(-1, 2, 2):
-rocket = turtle.Turtle()
-rocket.hideturtle()
-rocket.color('white')
-rocket.shape('square')
-rocket.shapesize(stretch_len=1, stretch_wid=5)
-rocket.up()
-rocket.goto(-450,0)
-rocket.showturtle()
-#    rockets.append(rocket)
+rocket_a = turtle.Turtle()
+rocket_a.hideturtle()
+rocket_a.color('white')
+rocket_a.shape('square')
+rocket_a.shapesize(stretch_len=1, stretch_wid=5)
+rocket_a.penup()
+rocket_a.goto(-450,0)
+rocket_a.showturtle()
+rocket_b = turtle.Turtle()
+rocket_b.hideturtle()
+rocket_b.color('white')
+rocket_b.shape('square')
+rocket_b.shapesize(stretch_len=1, stretch_wid=5)
+rocket_b.penup()
+rocket_b.goto(450,0)
+rocket_b.showturtle()
 
-def move_up():
-    y = rocket.ycor() + 10
+
+
+def move_up_a():
+    y = rocket_a.ycor() + 10
     if y > 250:
         y = 250
-    rocket.sety(y)
+    rocket_a.sety(y)
 
-def move_down():
-    y = rocket.ycor() - 10
+def move_down_a():
+    y = rocket_a.ycor() - 10
     if y < -250:
         y = -250
-    rocket.sety(y)
+    rocket_a.sety(y)
+
+def move_up_b():
+    y = rocket_b.ycor() + 10
+    if y > 250:
+        y = 250
+    rocket_b.sety(y)
+
+def move_down_b():
+    y = rocket_b.ycor() - 10
+    if y < -250:
+        y = -250
+    rocket_b.sety(y)
+
+ball = turtle.Turtle()
+ball.speed(0)
+ball.color('red')
+ball.shape('circle')
+ball.penup()
+ball.dx = 3
+ball.dy = 3
+
+
 
 
 winndow.listen()
-winndow.onkeypress(move_up(), "w")
-winndow.onkeypress(move_down(), "s")
-#winndow.onkeypress(move_up(rockets[1]), "Up")
-#winndow.onkeypress(move_down(rockets[1]), "Down")
+winndow.onkeypress(move_up_a, "w")
+winndow.onkeypress(move_down_a, "s")
+winndow.onkeypress(move_up_b, "Up")
+winndow.onkeypress(move_down_b, "Down")
+
+while True:
+    winndow.update
+    ball.setx(ball.xcor()+ball.dx)
+    ball.sety(ball.ycor()+ball.dy)
+
+    if ball.ycor() >= 290 or ball.ycor() <= -290:
+        ball.dy = -ball.dy
+    if ball.xcor() >= 490 or ball.xcor() <= -490:
+        ball.goto(0,random.randint(-150,150))
+        ball.dx = random.choice([-4,-3,-2,2,3,4])
+        ball.dy = random.choice([-4,-3,-2,2,3,4])
+
 winndow.mainloop()
