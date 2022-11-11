ab34509f0b357c9383c0521383a8240f997fde38
StepanTWL <h3xp3ctb@gmail.com>
2022-11-11 17:01:53 +0200

    11.11.2022 work

--- Homework/Homework14.py
+++ Homework/Homework14.py
@@ -301,7 +301,7 @@ for i in range(n):
         print(f'{arr[i]} - {arr[0]}')
     else:
         print(f'{arr[i]} - {arr[i+1]}')
-"""
+
 
 import random
 from string import ascii_uppercase, ascii_lowercase, digits
@@ -331,3 +331,50 @@ def generate_passwords(count: int, length: int) -> List[str]:
 n = int(input())
 m = int(input())
 print(*generate_passwords(n, m), sep='\n')
+
+
+import random
+
+n = 10**6       # количество испытаний
+m = 0
+for i in range(n):
+    x = random.uniform(-2.0, 2.0)
+    y = random.uniform(-2.0, 2.0)
+    if (-2 <= x <= 2) and (-2 <= y <= 2) and (x**3+y**4+2 >= 0) and (3*x+y**2 <= 2):
+        m += 1
+print(16*m/n)
+
+import random
+
+n = 10 ** 6  # количество испытаний
+m = 0
+for i in range(n):
+    x = random.uniform(-1.0, 1.0)
+    y = random.uniform(-1.0, 1.0)
+    if x ** 2 + y ** 2 <= 1:
+        m += 1
+print(4 * m / n)
+
+
+from decimal import *
+
+s = '0.77 4.03 9.06 3.80 7.08 5.88 0.23 4.65 2.79 0.90 4.23 2.15 3.24 8.57 0.10 8.57 1.49 5.64 3.63 8.36 1.56 6.67 1.46 5.26 4.83 7.23 1.22 1.02 7.82 9.97 5.40 9.79 9.82 2.78 2.96 0.07 1.72 7.24 7.84 9.23 1.71 6.24 5.78 5.37 0.03 9.60 8.86 2.73 5.83 6.50'
+arr = list(map(Decimal, s.split()))
+print(max(arr)+min(arr))
+
+
+from decimal import *
+s = '9.73 8.84 8.92 9.60 9.32 8.97 8.53 1.26 6.62 9.85 1.85 1.80 0.83 6.75 9.74 9.11 9.14 5.03 5.03 1.34 3.52 8.09 7.89 8.24 8.23 5.22 0.30 2.59 1.25 6.24 2.14 7.54 5.72 2.75 2.32 2.69 9.32 8.11 4.53 0.80 0.08 9.36 5.22 4.08 3.86 5.56 1.43 8.36 6.29 5.13'
+arr = list(map(Decimal, s.split()))
+arr = sorted(arr, reverse=True)
+print(sum(arr))
+print(*arr[:5])
+"""
+
+from decimal import *
+num = Decimal(input())
+if abs(num) > 1:
+    print(max(num.as_tuple().digits)+min(num.as_tuple().digits))
+else:
+    print(max(num.as_tuple().digits))
+pass
