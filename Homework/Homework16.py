ddb92c1f807b31e1bbc80c8e800569da669d16be
StepanTWL <h3xp3ctb@gmail.com>
2022-11-30 17:58:26 +0200

    30.11.2022 work

--- Homework/Homework16.py
+++ Homework/Homework16.py
@@ -54,4 +54,89 @@ def square(side):
 
 square(50)
 time.sleep(500)
+
+
+
+def greet(name, *args):
+    s = 'Hello, '+name
+    if not len(args):
+        return (s + '!')
+    s += ' and '
+    for i in args:
+        s += i + ' and '
+    s = s[:-5] + '!'
+    return s
+
+
+print(greet('Timur'))
+print(greet('Timur', 'Roman'))
+print(greet('Timur', 'Roman', 'Ruslan'))
+
+
+
+def print_products(*args):
+    count = 1
+    if not len(args):
+        print('Нет продуктов')
+        return
+    for i in args:
+        if isinstance(i, str) and not i=='':
+            print(f'{count}) {i}')
+            count += 1
+    if count == 1:
+        print('Нет продуктов')
+        return
+
+print_products('Бананы', [1, 2], ('Stepik',), 'Яблоки', '', 'Макароны', 5, True)
+print_products([4], {}, 1, 2, {'Beegeek'}, '')
+
+
+
+def info_kwargs(**kwargs):
+    for i in sorted(kwargs):
+        print(f'{i}: {kwargs[i]}')
+
+info_kwargs(first_name='Timur', last_name='Guev', age=28, job='teacher')
+
+
+def comparator(args):
+    if len(args):
+        return sum(args)/len(args)
+    return 0
+
+numbers = [(10, 10, 10), (30, 45, 56), (81, 39), (1, 2, 3), (12,), (-2, -4, 100), (1, 2, 99), (89, 9, 34), (10, 20, 30, -2), (50, 40, 50), (34, 78, 65), (-5, 90, -1, -5), (1, 2, 3, 4, 5, 6), (-9, 8, 4), (90, 1, -45, -21)]
+print(min(numbers, key=comparator))
+print(max(numbers, key=comparator))
+
+
+def comparator(point):
+    return point[0]**2+point[1]**2
+
+points = [(-1, 1), (5, 6), (12, 0), (4, 3), (0, 1), (-3, 2), (0, 0), (-1, 3), (2, 0), (3, 0), (-9, 1), (3, 6), (8, 8)]
+print(sorted(points, key=comparator))
+
+
+
+def comparator(data):
+    return min(data)+max(data)
+
+numbers = [(10, 10, 10), (30, 45, 56), (81, 80, 39), (1, 2, 3), (12, 45, 67), (-2, -4, 100), (1, 2, 99), (89, 90, 34), (10, 20, 30), (50, 40, 50), (34, 78, 65), (-5, 90, -1)]
+print(sorted(numbers, key=comparator))
+"""
+
+input().startswith('mam')
+
+
+
+"""
+def comparator_i(i):
+    def comparator(pair):
+        return pair[n-1]
+    return comparator
+
+athletes = [('Дима', 10, 130, 35), ('Тимур', 11, 135, 39), ('Руслан', 9, 140, 33), ('Рустам', 10, 128, 30),
+            ('Амир', 16, 170, 70), ('Рома', 16, 188, 100), ('Матвей', 17, 168, 68), ('Петя', 15, 190, 90)]
+
+command = int(input())-1
+[print(*t) for t in sorted(athletes, key=comparator_i[n])]
 """
---
+++ Lesson59_fractal.py
@@ -0,0 +1,316 @@
+"""
+gens = 10
+axiom = 'A'
+rules = {
+    'A': 'AB',
+    'B': 'A'
+}
+
+
+def apple_rules(s: str) -> str:
+    res = ''
+    for chr in s:
+        res += rules[chr]
+    return res
+
+
+for gen in range(gens):
+    print(f'generation {gen}: {axiom}')
+    axiom = apple_rules(axiom)
+
+
+import turtle
+
+# L-system setting
+gens = 18
+axiom = 'A'
+rules = {
+    'A': 'AB',
+    'B': 'A'
+}
+
+# screen setting
+WIDTH, HEIGHT = 1600, 900
+screen = turtle.Screen()
+screen.setup(WIDTH, HEIGHT)
+screen.screensize(3 * WIDTH, 3 * HEIGHT)  # размер поля в 9 раз больше окна
+screen.bgcolor('black')
+screen.delay(0)
+
+# turtle setting
+STEP, ANGLE = 50, 60
+leo = turtle.Turtle()
+leo.pensize(3)
+leo.speed(0)
+leo.color('orange')
+
+
+def apple_rules(s: str) -> str:
+    return ''.join([rules[chr] for chr in s])
+
+
+def get_result(gens, axiom):
+    for gen in range(gens):
+        axiom = apple_rules(axiom)
+    return axiom
+
+
+turtle.pencolor('white')
+turtle.goto(-WIDTH // 2 + 60, -HEIGHT // 2 + 60)
+turtle.clear()
+turtle.write(f'generation: {gens}', font=('Arial', 60, 'normal'))
+turtle.hideturtle()
+
+
+axiom = get_result(gens, axiom)
+for chr in axiom:
+    if chr == 'A':  # плохо сделано!!!
+        leo.left(ANGLE)
+        leo.forward(STEP)
+    else:
+        leo.right(ANGLE)
+        leo.forward(STEP)
+
+leo.hideturtle()
+screen.exitonclick()
+
+
+import turtle
+
+# L-system setting
+gens = 8
+axiom = 'F'
+chr_1, rule_1 = 'F', 'F-G+F+G-F'
+chr_2, rule_2 = 'G', 'GG'
+
+# screen setting
+WIDTH, HEIGHT = 1600, 900
+screen = turtle.Screen()
+screen.setup(WIDTH, HEIGHT)
+screen.screensize(3 * WIDTH, 3 * HEIGHT)  # размер поля в 9 раз больше окна
+screen.bgcolor('black')
+screen.delay(0)
+
+# turtle setting
+STEP, ANGLE = 1, 120
+leo = turtle.Turtle()
+leo.pensize(3)
+leo.speed(0)
+leo.setpos(-WIDTH//3, -HEIGHT//2)
+leo.color('green')
+
+
+
+def apple_rules(s: str) -> str:
+    return ''.join([rule_1 if chr == chr_1 else rule_2 if chr == chr_2 else chr for chr in s])
+
+
+def get_result(gens, axiom):
+    for gen in range(gens):
+        axiom = apple_rules(axiom)
+    return axiom
+
+
+turtle.pencolor('white')
+turtle.goto(-WIDTH // 2 + 60, HEIGHT // 2 - 100)
+turtle.clear()
+turtle.write(f'generation: {gens}', font=('Arial', 60, 'normal'))
+#turtle.hideturtle()
+
+
+axiom = get_result(gens, axiom)
+for chr in axiom:
+    if chr == chr_1 or chr == chr_2:
+        leo.forward(STEP)
+    elif chr == '+':
+        leo.right(ANGLE)
+    elif chr == '-':
+        leo.left(ANGLE)
+
+
+screen.exitonclick()
+
+
+#dragon
+import turtle
+
+# L-system setting
+gens = 13
+axiom = 'XY'
+chr_1, rule_1 = 'X', 'X+YF+'
+chr_2, rule_2 = 'Y', '-FX-Y'
+
+# screen setting
+WIDTH, HEIGHT = 1600, 900
+screen = turtle.Screen()
+screen.setup(WIDTH, HEIGHT)
+screen.screensize(3 * WIDTH, 3 * HEIGHT)  # размер поля в 9 раз больше окна
+screen.bgcolor('black')
+screen.delay(0)
+
+# turtle setting
+STEP, ANGLE = 4, 90
+leo = turtle.Turtle()
+leo.pensize(3)
+leo.speed(0)
+leo.setpos(WIDTH//4, -HEIGHT//4-25)
+leo.color('green')
+
+
+
+def apple_rules(s: str) -> str:
+    return ''.join([rule_1 if chr == chr_1 else rule_2 if chr == chr_2 else chr for chr in s])
+
+
+def get_result(gens, axiom):
+    for gen in range(gens):
+        axiom = apple_rules(axiom)
+    return axiom
+
+
+turtle.pencolor('white')
+turtle.goto(-WIDTH // 2 + 60, HEIGHT // 2 - 100)
+turtle.clear()
+turtle.write(f'generation: {gens}', font=('Arial', 60, 'normal'))
+#turtle.hideturtle()
+
+
+axiom = get_result(gens, axiom)
+for chr in axiom:
+    if chr == chr_1 or chr == chr_2:
+        leo.forward(STEP)
+    elif chr == '+':
+        leo.right(ANGLE)
+    elif chr == '-':
+        leo.left(ANGLE)
+
+
+screen.exitonclick()
+
+
+
+#снежинка коха
+import turtle
+
+# L-system setting
+gens = 6
+axiom = 'F++F++F'
+chr_1, rule_1 = 'F', 'F-F++F-F'
+
+# screen setting
+WIDTH, HEIGHT = 1600, 900
+screen = turtle.Screen()
+screen.setup(WIDTH, HEIGHT)
+screen.screensize(3 * WIDTH, 3 * HEIGHT)  # размер поля в 9 раз больше окна
+screen.bgcolor('black')
+screen.delay(0)
+
+# turtle setting
+STEP, ANGLE = 1, 60
+leo = turtle.Turtle()
+leo.pensize(3)
+leo.speed(0)
+leo.setpos(-WIDTH//6, HEIGHT//6)
+leo.color('green')
+
+
+
+def apple_rules(s: str) -> str:
+    return ''.join([rule_1 if chr == chr_1 else chr for chr in s])
+
+
+def get_result(gens, axiom):
+    for gen in range(gens):
+        axiom = apple_rules(axiom)
+    return axiom
+
+
+turtle.pencolor('white')
+turtle.goto(-WIDTH // 2 + 60, HEIGHT // 2 - 100)
+turtle.clear()
+turtle.write(f'generation: {gens}', font=('Arial', 60, 'normal'))
+#turtle.hideturtle()
+
+
+axiom = get_result(gens, axiom)
+for chr in axiom:
+    if chr == chr_1:
+        leo.forward(STEP)
+    elif chr == '+':
+        leo.right(ANGLE)
+    elif chr == '-':
+        leo.left(ANGLE)
+
+
+screen.exitonclick()
+"""
+
+
+
+
+
+#Ветка
+import turtle
+
+# L-system setting
+gens = 6
+axiom = 'XY'
+chr_1, rule_1 = 'F', 'FF'
+chr_2, rule_2 = 'X', 'F[+X]F[-X]+X'
+
+# screen setting
+WIDTH, HEIGHT = 1600, 900
+screen = turtle.Screen()
+screen.setup(WIDTH, HEIGHT)
+screen.screensize(3 * WIDTH, 3 * HEIGHT)  # размер поля в 9 раз больше окна
+screen.bgcolor('black')
+screen.delay(0)
+
+# turtle setting
+STEP, ANGLE = 7, 22.5
+stack = []
+leo = turtle.Turtle()
+leo.pensize(3)
+leo.speed(0)
+leo.setpos(0, -HEIGHT//2)
+leo.color('green')
+
+
+
+def apple_rules(s: str) -> str:
+    return ''.join([rule_1 if chr == chr_1 else chr for chr in s])
+
+
+def get_result(gens, axiom):
+    for gen in range(gens):
+        axiom = apple_rules(axiom)
+    return axiom
+
+
+turtle.pencolor('white')
+turtle.goto(-WIDTH // 2 + 60, HEIGHT // 2 - 100)
+turtle.clear()
+turtle.write(f'generation: {gens}', font=('Arial', 60, 'normal'))
+#turtle.hideturtle()
+
+
+axiom = get_result(gens, axiom)
+leo.left(90)
+for chr in axiom:
+    if chr == chr_1:
+        leo.forward(STEP)
+    elif chr == '+':
+        leo.right(ANGLE)
+    elif chr == '-':
+        leo.left(ANGLE)
+    elif chr == '[':
+        angle_, pos_ = leo.heading(), leo.pos()
+        stack.append((angle_, pos_))
+    elif chr == ']':
+        angle_, pos_ = stack.pop()
+        leo.setheading(angle_)
+        leo.penup()
+        leo.goto(pos_)
+        leo.pendown()
+
+screen.exitonclick()
