ea240db0e8e71b7b2eb0457f20f93ac35aed415f
StepanTWL <h3xp3ctb@gmail.com>
2022-12-09 17:03:12 +0200

    09.12.2022 work

--- Homework/Homework16.py
+++ Homework/Homework16.py
@@ -54,4 +54,61 @@ def square(side):
 
 square(50)
 time.sleep(500)
+
+
+s=input().lower()
+if s[:3] == 'mam':
+    print('True')
+else:
+    print('False')
+
+
+
+s=input().lower()
+s1=input().lower()
+if s[-len(s1):] == s1:
+    print("True")
+else:
+    print("False")
+
+
+
+s=input()
+s1=input()
+s2=input()
+if s[-len(s2):] == s2 and s[:len(s1)] == s1:
+    print("True")
+else:
+    print("False")
+
+
+r=int(input())
+g=int(input())
+b=int(input())
+
+s='#'+hex(r)[2:].zfill(2).up+hex(g)[2:].zfill(2)+hex(b)[2:].zfill(2)
+
+
+
+my_list = [['q', 'w', 't'] for _ in range(15)]
+my_list = ['q', 'w', 't']*15
+
+numbers = [-214, 181, -139, 448, -20, -917, 32, 422, -895, 198, 284, 472, -986, -964, -989, 29]
+numbers.sort(reverse=True)
+
+my_list = list(map(int, input().split()))
+if 3 in my_list:
+    my_list.remove(3)
+if 5 in my_list:
+    my_list.remove(5)
+if 7 in my_list:
+    my_list.remove(7)
+if 9 in my_list:
+    my_list.remove(9)
+print(my_list)
 """
+
+s=list(map(str, input().split()))
+
+
+pass
